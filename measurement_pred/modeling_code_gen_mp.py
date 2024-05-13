14535bf04c9cc249dc1333a3c8b0bb7db1e19874
oliveradk <danielskoch.oliver@gmail.com>
2024-05-13 12:04:35 -0700

    stuff for slurm

--- measurement_pred/modeling_code_gen_mp.py
+++ measurement_pred/modeling_code_gen_mp.py
@@ -1,4 +1,4 @@
-from transformers.models.codegen import CodeGenPreTrainedModel
+from transformers.models.codegen import CodeGenPreTrainedModel, CodeGenModel
 
 from .modeling_mp import MeasurementPredictorMixin
 from .configuration_code_gen_mp import CodeGenMeasurementPredictorConfig
@@ -6,3 +6,8 @@ from .configuration_code_gen_mp import CodeGenMeasurementPredictorConfig
 
 class CodeGenMeasurementPredictor(CodeGenPreTrainedModel, MeasurementPredictorMixin):
     config_class = CodeGenMeasurementPredictorConfig
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.transformer = CodeGenModel(config)
+        self.post_init()
