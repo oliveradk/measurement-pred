b660638ca0f38a2c39e7e2d75a9f76c28488effe
Oliver Daniels-Koch <danielskoch.oliver@gmail.com>
2024-05-22 08:27:55 -0400

    set use_cache to false (fixed bug in codegen eval)

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class ModelTypes(Enum):
     CODEGEN = "codegen"
     GPT_NEOX = "gpt_neox"
 
-KEYS_TO_REMOVE = ("_name_or_path",)
+KEYS_TO_REMOVE = ("_name_or_path", "use_cache")
 
 def filter_config(config: dict):
     return {k: v for k, v in config.items() if k not in KEYS_TO_REMOVE}
@@ -20,14 +20,14 @@ def load_model(model_type: ModelTypes, pretrained_model_name: str):
     if model_type in (ModelTypes.CODEGEN, ModelTypes.CODEGEN.value):
         CodeGenMeasurementPredictorConfig.register_for_auto_class()
         CodeGenMeasurementPredictor.register_for_auto_class("AutoModelForSequenceClassification")
-        config = CodeGenMeasurementPredictorConfig(**filter_config(AutoConfig.from_pretrained(pretrained_model_name).to_dict()))
+        config = CodeGenMeasurementPredictorConfig(**filter_config(AutoConfig.from_pretrained(pretrained_model_name).to_dict()), use_cache=False)
         model = CodeGenMeasurementPredictor(config)
         tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name, padding_side="left", truncation_side="left") 
         return config, model, tokenizer
     elif model_type in (ModelTypes.GPT_NEOX, ModelTypes.GPT_NEOX.value):
         GPTNeoXMeasurementPredictorConfig.register_for_auto_class()
         GPTNeoXMeasurementPredictor.register_for_auto_class("AutoModelForSequenceClassification")
-        config = GPTNeoXMeasurementPredictorConfig(**filter_config(AutoConfig.from_pretrained(pretrained_model_name).to_dict()))
+        config = GPTNeoXMeasurementPredictorConfig(**filter_config(AutoConfig.from_pretrained(pretrained_model_name).to_dict()), use_cache=False)
         model = GPTNeoXMeasurementPredictor(config)
         tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name, padding_side="left", truncation_side="left")
         return config, model, tokenizer
